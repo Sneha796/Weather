c9f1df7f21d51c7cdd0e6257f7b97fd573cfd241
Sneha <snehanikam796@gmail.com>
2026-08-30 23:51:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+const cityName=
+  document.querySelector(".search") 
+
+const forecast=
+  document.querySelector(".forecast") 
+
+let city=
+  document.getElementById("search-btn") 
+
+let location=
+  document.getElementById("location") 
+
+let celsius=
+  document.getElementById("celsius") 
+
+let fahrenheit=
+  document.getElementById("fahrenheit") 
+
+let temperature=
+  document.getElementById("temperature") 
+
+let condition=
+  document.getElementById("condition1") 
+
+let humidity=
+  document.getElementById("humidity")
+
+let wind=
+  document.getElementById("wind") 
+
+let precipitation=
+  document.getElementById("precipitation") 
 
